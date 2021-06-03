8511368ca7cd0be088d94dd2796fcdcdcac7589d
Henry Ambler <60691073+hen-rr12@users.noreply.github.com>
2021-06-03 13:30:48 +0100

    intial commit

--- parser.py
+++ parser.py
@@ -1,3 +1,11 @@
-import request
+import requests
+from lxml import etree
+import urllib
 
-rss_url = "https://www.world-nuclear-news.org/?rss=feed"
+url = "https://www.world-nuclear-news.org/?rss=feed"
+root = etree.parse(urllib.request.urlopen(url))
+
+for obs in root.xpath('rss//channel//title'):
+    print(obs)
+    price = obs.xpath('./base:OBS_VALUE').text
+    print(price)
